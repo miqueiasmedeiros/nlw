7dd755d7851a73b1ccfc573196e199c6057decc0
Miquéias Medeiros <90843616+miqueiasmedeiros@users.noreply.github.com>
2022-11-05 22:51:45 -0300

    :tada: initial commit

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ function createGame(player1, hour, player2) {
         </li>
   `
 }
-
+let delay = -0.5;
 function createCard(date, day, games) {
-  return ` <div class="card">
+  delay = delay + 0.5;
+  return ` <div class="card" style= "animation-delay: ${delay}s">
             <h2>${date} <span>${day}</span></h2>
             <ul>
             ${games}                              
@@ -17,34 +18,28 @@ function createCard(date, day, games) {
         `
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-        <img src="./assets/logo.svg" alt="Logo NLW">
-    </header>
-    <main id="cards">
-       ${createCard(
+document.querySelector("#cards").innerHTML =   
+       createCard(
          "24/11",
          "quinta",
          createGame("switzerland", "07:00", "cameroon") +
            createGame("uruguay", "10:00", "korea") +
            createGame("portugal", "13:00", "ghana") +
            createGame("brazil", "16:00", "serbia")
-       )}   
-       ${createCard(
+       ) +
+       createCard(
          "28/11",
          "segunda",
          createGame("cameroon", "07:00", "serbia") +
            createGame("korea", "10:00", "ghana") +
            createGame("brazil", "13:00", "switzerland") +
            createGame("portugal", "16:00", "uruguay")
-       )}   
-       ${createCard(
+       ) +
+       createCard(
          "02/12",
          "sexta",
-         createGame("korea", "12:00", "portugal")+
-         createGame("ghana", "12:00", "uruguay")+
-         createGame("serbia", "16:00", "switzerland")+
-         createGame("brazil", "16:00", "cameroon")
-       )}        
-    </main>
-    `
+         createGame("korea", "12:00", "portugal") +
+           createGame("ghana", "12:00", "uruguay") +
+           createGame("serbia", "16:00", "switzerland") +
+           createGame("brazil", "16:00", "cameroon")
+       )
